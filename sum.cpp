d002b521d8736c7fd3f9d1b9474adc2a35b4e7b3
Abdul-Wahab <165203959+whizz2017@users.noreply.github.com>
2024-11-04 00:28:13 +0500

    Extensive Practice on Declaring, Initialising & printing the Value of Variables 03-Nov-2024
    Abdul Wahab  in C++, has practised declaring and initialising variables as well as displaying the values of variables on the command prompt. He has also learned how to use the command prompt to compile and run programs. Additionally, he learned how our program handles comments today (03-Nov-2024).

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+ int main () {
+
+	cout<<"Hello Abdul Wahab"<<endl;
+	// declare variable
+	int x,y,z;
+	//inistlice
+	x=50;
+	y=23;
+	z=x-y;
+	//print the value
+	cout<<"The Value of z is : "<< z <<endl;
+return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+ int main () {
+
+	cout<<"Hello Abdul Wahab"<<endl;
+	
+	// Declare & initialize int variable colorPenciles
+	int colorPenciles;
+	colorPenciles = 12;
+ 	cout<<colorPenciles<<endl;	
+
+	
+	//Declare & initialize age;
+	int age;
+	age =7;
+     
+	cout<<age<<endl;	
+	
+	//Declare & initialize houseNumber
+ 	int houseNumber;
+	houseNumber = 121;
+
+	cout<<houseNumber<<endl;
+	
+	return 0;
+}
